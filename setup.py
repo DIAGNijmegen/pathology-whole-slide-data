fcf38787d8ba0d8e22f26962f15747da3215eaf8
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-09-19 12:46:09 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="wholeslidedata",
-    version="0.0.1",
+    version="0.0.3",
     author="Mart van Rijthoven",
     author_email="mart.vanrijthoven@gmail.com",
-    packages=["wholeslidedata"],
+    packages=find_packages(exclude=("tests",'notebooks')),
     url="http://pypi.python.org/pypi/wholeslidedata/",
     license="LICENSE.txt",
     install_requires=[
         "concurrentbuffer>=0.0.3",
-        "creationism>=0.0.2",
+        "creationism>=0.0.3",
         "numpy>=1.18.1",
         "opencv-python>=4.4.0",
         "scipy>=1.5.2",
         "scikit-image>=0.17.2",
         "shapely>=1.7.1",
-        "openslide>=1.1.1",
+        "openslide-python>=1.1.1",
         "PyYAML>=5.4.1"
     ],
     long_description="Package for working with whole slide images.",
