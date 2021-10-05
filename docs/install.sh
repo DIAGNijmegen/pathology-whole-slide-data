df0d255dd21865e306c6c304cc2cb38ed94602c2
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-10-05 08:03:06 +0200

    installatiosn for docs

--- docs/install.sh
+++ docs/install.sh
@@ -3,11 +3,11 @@
 export asap_version=1.9
 export asap_deb=ASAP-${asap_version}-Linux-Ubuntu1804.deb
 curl --remote-name --location https://github.com/computationalpathologygroup/ASAP/releases/download/${asap_version}/${asap_deb}
-apt-get update
-dpkg --install ${asap_deb} || true
-apt-get install --fix-broken --assume-yes
-ldconfig -v 
+sudo apt-get update
+sudo dpkg --install ${asap_deb} || true
+sudo apt-get install --fix-broken --assume-yes
+sudo ldconfig -v 
 echo python -m site
 echo "/opt/ASAP/bin" > /usr/local/lib/python3.8/dist-packages/asap.pth 
 
-apt-get install openslide-tools
+sudo apt-get install openslide-tools
