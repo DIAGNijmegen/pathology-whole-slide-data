53619e457f7469da05cefd52e48366651e9b2665
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-11-30 14:18:30 +0100

    configuration for batch reference sampler

--- wholeslidedata/buffer/batchcommander.py
+++ wholeslidedata/buffer/batchcommander.py
@@ -1,36 +1,30 @@
-from collections import deque
-from wholeslidedata.samplers.batchreferencesampler import BatchReferenceSampler
-from concurrentbuffer.commander import Commander
-from queue import Empty
-MESSAGE_MODE_IDENTIFIER = 'mode'
-MESSAGE_SAMPLE_REFERENCES_IDENTIFIER = 'sample_references'
-MESSAGE_INDEX_IDENTIFIER = 'index'
-import time
 import copy
+from queue import Empty
+
+from concurrentbuffer.commander import Commander
+
+MESSAGE_MODE_IDENTIFIER = "mode"
+MESSAGE_SAMPLE_REFERENCES_IDENTIFIER = "sample_references"
+MESSAGE_INDEX_IDENTIFIER = "index"
+
+
 class BatchCommander(Commander):
-    def __init__(self, config_builder, mode, reset_index=None, update_queue=None, info_queue=None):
+    def __init__(
+        self, config_builder, mode, reset_index=None, update_queue=None, info_queue=None
+    ):
         self._config_builder = config_builder
         self._mode = mode
         self._batch_reference_sampler = None
         self._reset_index = reset_index
         self._index = 0
-        self._update_queue =update_queue
+        self._update_queue = update_queue
         self._info_queue = info_queue
 
     def build(self):
         mode = self._mode
         self._index = 0
         build = self._config_builder.build_instances()
-
-        self._dataset=build["wholeslidedata"][mode]["dataset"]
-        self._batch_size=build["wholeslidedata"][mode]["batch_shape"].batch_size
-        self._label_sampler=build["wholeslidedata"][mode]["label_sampler"]
-        self._annotation_sampler=build["wholeslidedata"][mode]["annotation_sampler"]
-        self._point_sampler=build["wholeslidedata"][mode]["point_sampler"]
-
-        self._batch_reference_sampler = BatchReferenceSampler(
-            self._dataset, self._batch_size, self._label_sampler, self._annotation_sampler, self._point_sampler
-        )
+        self._batch_reference_sampler = build["wholeslidedata"][mode]["batch_reference_sampler"]
 
     def create_message(self) -> dict:
         self._update()
@@ -44,7 +38,7 @@ class BatchCommander(Commander):
         }
         if self._info_queue:
             self._info_queue.put(copy.deepcopy(message))
-        self._index += 1 
+        self._index += 1
         return message
 
     def _update(self):
@@ -60,6 +54,6 @@ class BatchCommander(Commander):
     def _reset(self):
         if self._reset_index is None:
             return
-        
+
         if self._index % self._reset_index == 0:
             self._batch_reference_sampler.reset()
