8ac74bceded8e30e4b732e64f60d267e734638a2
thijsgelton <thijs.gelton@hotmail.com>
2022-02-09 20:39:06 +0100

    Added colors to both the parsing and converting (groups and annotation)

--- tests/annotation_test.py
+++ tests/annotation_test.py
@@ -81,3 +81,50 @@ class TestAnnotation:
         assert polygon.contains(point)
         polygon.add_overlapping_annotations([polygon2])
         assert not polygon.contains(point)
+
+
+    def test_parse_colour_from_annotation(self):
+        xml_string = """<?xml version="1.0"?>
+        <ASAP_Annotations>
+            <Annotations>
+                <Annotation Name="test_annotation" Type="Rectangle" PartOfGroup="test" Color="#F4FA58">
+                    <Coordinates>
+                        <Coordinate Order="0" X="70316.8047" Y="94614.7422" />
+                        <Coordinate Order="1" X="72576.6484" Y="94614.7422" />
+                        <Coordinate Order="2" X="72576.6484" Y="96517.4375" />
+                        <Coordinate Order="3" X="70316.8047" Y="96517.4375" />
+                    </Coordinates>
+                </Annotation>
+                </Annotations>
+            <AnnotationGroups>
+                <Group Name="test" PartOfGroup="None" Color="#ff0000"> <Attributes /> </Group>
+            </AnnotationGroups>
+        </ASAP_Annotations>
+        """
+        dir_path = tempfile.mkdtemp()
+        test_annotation_path = os.path.join(dir_path, "test_annotation.xml")
+        with open(test_annotation_path, "w") as xml:
+            xml.write(xml_string)
+
+        wsa = WholeSlideAnnotation(test_annotation_path)
+        self.assertIsNotNone(wsa)
+        self.assertEqual(wsa.annotations[0].label.color, "#F4FA58")
+        shutil.rmtree(dir_path)
+
+    def test_parse_colour_from_annotations_xml(self):
+        dir_path = tempfile.mkdtemp()
+        annotation_path = download_wsa(dir_path)
+        wsa = WholeSlideAnnotation(annotation_path)
+        self.assertIsNotNone(wsa)
+        self.assertRegex(wsa.annotations[0].label.color, r"^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$")
+        shutil.rmtree(dir_path)
+
+    def test_convert_annotations(self):
+        dir_path = tempfile.mkdtemp()
+        annotation_path = download_wsa(dir_path)
+        wsa = WholeSlideAnnotation(annotation_path)
+        coordinates = wsa.annotations[0].coordinates[0]
+        self.assertIsNotNone(wsa)
+        convert_annotations(Path(annotation_path).parent, Path(annotation_path).parent, scaling=2.0)
+        wsa_changed = WholeSlideAnnotation(annotation_path)
+        self.assertTrue(np.allclose(coordinates, wsa_changed.annotations[0].coordinates[0] / 2.0, rtol=1e-1))
