4a3ab26f2f34472fa3827a5959b597f6de643129
thijsgelton <thijs.gelton@hotmail.com>
2022-03-22 09:49:04 +0100

    Separate sampler so no augmentation is already applied

--- tests/callbacks.py
+++ tests/callbacks.py
@@ -2,10 +2,12 @@ import os
 import unittest
 from pathlib import Path
 
+import numpy as np
 from shapely.geometry import Point
 
 from tests.testdata import download_wsi, download_wsa
 from wholeslidedata.configuration.config import WholeSlideDataConfiguration
+from wholeslidedata.samplers.patchlabelsampler import SegmentationPatchLabelSampler
 from wholeslidedata.samplers.utils import plot_patch, plot_mask
 from wholeslidedata.source.utils import whole_slide_files_from_folder_factory
 
@@ -20,15 +22,15 @@ class TestsConfigurations(unittest.TestCase):
 
         config_builder = WholeSlideDataConfiguration.build(user_config=config_file, modes=("training",))
 
-        wsi = whole_slide_files_from_folder_factory(test_files, 'wsi', excludes=('mask',), image_backend='asap')[0]
-        wsa = whole_slide_files_from_folder_factory(test_files, 'wsa', annotation_parser='asap')[0]
-        x_samples, y_samples = config_builder['wholeslidedata']['training']['sample_sampler'].sample(wsi.open(),
-                                                                                                     wsa.open(),
-                                                                                                     point=Point(15770,
-                                                                                                                 13260))
-        x = x_samples[.5][(284, 284, 3)]
-        y = y_samples[.5][(284, 284, 3)]
+        wsi = whole_slide_files_from_folder_factory(test_files, 'wsi', excludes=('mask',), image_backend='openslide')[
+            0].open()
+        wsa = whole_slide_files_from_folder_factory(test_files, 'wsa', annotation_parser='asap')[0].open()
+        x = wsi.get_patch(x=15770, y=13260, width=284, height=284, spacing=0.5)
+        y = SegmentationPatchLabelSampler().sample(wsa, point=Point(15770, 13260), size=(284, 284), ratio=1)
         aug_x, aug_y = config_builder['wholeslidedata']['training']['sample_callbacks'][1](x, y)
+
+        print(x.mean(), aug_x.mean())
+
         plot_patch(x)
         plot_patch(aug_x)
         plot_mask(y)
