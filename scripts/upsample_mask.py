06243f5a1d80c9734735934e310be92e27455143
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-04 15:58:30 +0200

    patch iterator and upsample masks

--- scripts/upsample_mask.py
+++ scripts/upsample_mask.py
@@ -57,7 +57,7 @@ def upscale_mask(mask_path, mask_spacing, output_spacing, cpus):
 
 @click.command()
 @click.option("--mask_folder", type=Path, required=True)
-@click.option("--suffix", type=Path, required=True)
+@click.option("--suffix", type=str, required=True)
 @click.option("--mask_spacing", type=float, required=True)
 @click.option("--output_spacing", type=float, required=True)
 @click.option("--cpus", type=int, required=True)
