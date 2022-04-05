df723cff248a9551abada88850f5e9948b00f646
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-05 15:51:56 +0200

    v0.0.13

--- setup.py
+++ setup.py
@@ -13,6 +13,17 @@ setup(
     packages=find_packages(exclude=("tests", "notebooks", "docs")),
     url="https://github.com/DIAGNijmegen/pathology-whole-slide-data",
     license="LICENSE.txt",
-    install_requires=[package.strip() for package in open("./requirements.txt").readlines()],
+    install_requires=[
+        "concurrentbuffer>=0.0.7",
+        "creationism>=0.0.5",
+        "numpy>=1.20.2",
+        "opencv-python-headless>=4.4.0",
+        "scipy>=1.5.2",
+        "scikit-image>=0.17.2",
+        "shapely>=1.7.1",
+        "openslide-python>=1.1.1",
+        "PyYAML>=5.4.1",
+        "jsonschema>=4.4.0",
+    ],
     long_description="Package for working with whole slide images.",
 )
