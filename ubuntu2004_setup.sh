3e2e741f932f197f374d37b14e438e94343a7b5b
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-12 20:15:18 +0200

    update ubuntu2004 setup

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -3,6 +3,9 @@
 apt-get update
 apt-get install openslide-tools
 pip install gdown
+pip install black
+pip install --upgrade --no-cache-dir gdown
 pip install wholeslidedata
-python ./downloaddata.py
+python ./pathology-whole-slide-data/downloaddata.py
+cp ./pathology-whole-slide-data/docs/userguide/configs/ ./
 
