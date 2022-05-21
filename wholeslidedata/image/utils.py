01752730cffe8869553f877bbab7d7a87110fc1e
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-05-21 11:02:02 +0200

    create thumbnail

--- wholeslidedata/image/utils.py
+++ wholeslidedata/image/utils.py
@@ -1,4 +1,7 @@
+import cv2
+
 from bisect import bisect_left
+
 
 def take_closest_level(spacings, spacing):
     pos = bisect_left(spacings, spacing)
@@ -9,3 +12,9 @@ def take_closest_level(spacings, spacing):
     if spacings[pos] - spacing < spacing - spacings[pos - 1]:
         return pos
     return pos - 1
+
+
+def create_thumbnail(wsi, output_folder, spacing=8.0):
+    slide = wsi.get_slide(spacing)
+    output_path = output_folder / (wsi.path.stem + ".png")
+    cv2.imwrite(str(output_path), slide)
