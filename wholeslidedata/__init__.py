f8db72300731c5c19f6557f394b864ab3faacede
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-05-21 10:53:00 +0200

    easy import wholeslideimage & wholeslideannotation

--- wholeslidedata/__init__.py
+++ wholeslidedata/__init__.py
@@ -1,4 +1,7 @@
 from wholeslidedata.version import __version__
 
+from wholeslidedata.image.wholeslideimage import WholeSlideImage
+from wholeslidedata.annotation.wholeslideannotation import WholeSlideAnnotation
+
 # load accessories
 from wholeslidedata import accessories as _
