c9d82f4e3e1075711c8cdbb5c1479d585749f3b1
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-05-27 18:45:54 +0200

    Update iterators.py

--- wholeslidedata/iterators.py
+++ wholeslidedata/iterators.py
@@ -99,7 +99,7 @@ class PatchBufferIterator(BufferIterator):
 
 def get_number_of_batches(number_of_batches, total_annotations, batch_size):
     if number_of_batches == -1:
-        number_of_batches = math.ceil(total_annotations / batch_size)
+        number_of_batches = math.ceil(total_annotations / batch_size) - 1
         redundant = batch_size - (total_annotations % batch_size)
         return number_of_batches, redundant
 
