cc35398373c988e0f58d6609ca0767f3b76af77d
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-08-01 10:17:46 +0200

    add writer factory functions

--- wholeslidedata/accessories/asap/imagewriter.py
+++ wholeslidedata/accessories/asap/imagewriter.py
@@ -1,10 +1,14 @@
 import abc
+from enum import Enum, auto
 from pathlib import Path
+from shutil import copyfile
+from typing import List
 
 import multiresolutionimageinterface as mir
 import numpy as np
 from multiresolutionimageinterface import MultiResolutionImageWriter
 from shapely import geometry
+from wholeslidedata.image.wholeslideimage import WholeSlideImage
 from wholeslidedata.samplers.patchlabelsampler import SegmentationPatchLabelSampler
 
 
@@ -164,6 +168,152 @@ class WholeSlideImageWriter(WholeSlideImageWriterBase):
         pixel_size_vec.push_back(self._spacing)
         self.setSpacing(pixel_size_vec)
         self.writeImageInformation(self._dimensions[0], self._dimensions[1])
+
+
+class MaskType(Enum):
+    """Different mask types
+
+    The PREDICTION type is for writing masks with prediction values, range=(0, num_classes)
+    The HEATMAP type is for writing masks with heatmap values, range=(0, 255)
+    """
+
+    PREDICTION = auto()
+    HEATMAP = auto()
+
+
+class TmpWholeSlideMaskWriter(WholeSlideMaskWriter):
+    def __init__(self, output_path: Path, callbacks=(), suffix=".tif"):
+        """Writes temp file and copies the tmp file to an output folder in the save method.
+        Args:
+            output_path (Path): path to copy the writed file when saving.
+        """
+
+        self._output_path = output_path
+        super().__init__(callbacks=callbacks, suffix=suffix)
+
+    def save(self):
+        super().save()
+        self._copy_temp_path_to_output_path()
+
+    def _copy_temp_path_to_output_path(self):
+        print(f"Copying from: {self._path}")
+        print(f"Copying to: {self._output_path}")
+        copyfile(self._path, self._output_path)
+        print("Removing tmp file...")
+        Path(self._path).unlink()
+        print(f"Copying done.")
+
+
+def _create_writer(
+    file: dict,
+    output_folder: Path,
+    tmp_folder: Path,
+    real_spacing: float,
+    shape: tuple,
+    tile_size: int,
+) -> TmpWholeSlideMaskWriter:
+    """Creates a writer
+    Args:
+        file (dict): dictionary containing a 'name' and 'type' key.
+        output_folder (Path): folder in where output should be copied
+        tmp_folder (Path): folder in where output should be kept temporary when writing
+        real_spacing (float): The spacing of the output file
+        shape (tuple): The shape of the ouput file
+    Raises:
+        ValueError: raises when type in file is not valid
+    Returns:
+        TmpWholeSlideMaskWriter: a writer
+    """
+
+    if file["type"] == MaskType.HEATMAP:
+        callbacks = (HeatmapTileCallback(heatmap_index=file["heatmap_index"]),)
+    elif file["type"] == MaskType.PREDICTION:
+        callbacks = (PredictionTileCallback(),)
+    else:
+        raise ValueError(f"Invalid file type: {file['type']}")
+
+    writer = TmpWholeSlideMaskWriter(
+        output_path=(output_folder / file["name"]), callbacks=callbacks
+    )
+    print(f'write: {(tmp_folder / file["name"])}')
+    writer.write(
+        path=(tmp_folder / file["name"]),
+        spacing=real_spacing,
+        dimensions=shape,
+        tile_shape=(tile_size, tile_size),
+    )
+    return writer
+
+
+def get_files(image_path, model_name, heatmaps):
+    files = []
+    # prediction
+    prediction_file_name = image_path.stem + f"_{model_name}.tif"
+    files.append({"name": prediction_file_name, "type": MaskType.PREDICTION})
+    # heatmaps
+    if heatmaps is not None:
+        for value in heatmaps:
+            heatmap_file_name = image_path.stem + f"_{model_name}_heat{value}.tif"
+            files.append(
+                {
+                    "name": heatmap_file_name,
+                    "type": MaskType.HEATMAP,
+                    "heatmap_index": value,
+                }
+            )
+
+    return files
+
+
+def create_writers(
+    image_path: Path,
+    model_name: str,
+    heatmaps: List[int],
+    spacing: float,
+    tile_size: int,
+    output_folder: Path,
+    tmp_folder: Path,
+) -> list:
+
+    """Creates writers
+    Args:
+        image_path (Path): path to the images that is being processed
+        model_name (str): name of the model
+        heatmaps (List): indices for heatmaps
+        spacing: float:
+        tile_sizeL int:
+        output_folder (Path): folder in where output should be copied
+        tmp_folder (Path): folder in where output should be kept temporary when writing
+    Returns:
+        list: the created writers
+    """
+
+    files = get_files(image_path=image_path, model_name=model_name, heatmaps=heatmaps)
+
+    writers = []
+
+    # get info
+    with WholeSlideImage(image_path) as wsi:
+        shape = wsi.shapes[wsi.get_level_from_spacing(spacing)]
+        real_spacing = wsi.get_real_spacing(spacing)
+
+    for file in files:
+        if (output_folder / file["name"]).exists():
+            f"Skipping prediction for {file['name']}, already exists in output folder: {output_folder}"
+            continue
+
+        writers.append(
+            _create_writer(
+                file=file,
+                output_folder=output_folder,
+                tmp_folder=tmp_folder,
+                real_spacing=real_spacing,
+                shape=shape,
+                tile_size=tile_size,
+            )
+        )
+
+    return writers
 
 
 def write_mask(wsi, wsa, spacing, tile_size=1024, suffix="_gt_mask.tif"):
