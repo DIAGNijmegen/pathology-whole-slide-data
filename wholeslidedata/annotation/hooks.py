4cc3c58b1d020e6c172970a33bdb6c89af4b5461
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-08-10 16:02:20 +0200

    fix/improve slidingwindowhook

--- wholeslidedata/annotation/hooks.py
+++ wholeslidedata/annotation/hooks.py
@@ -29,30 +29,31 @@ class ScalingAnnotationHook(AnnotationHook):
 @AnnotationHook.register(('tiles', 'tiled'))
 class TiledAnnotationHook(AnnotationHook):
 
-    def __init__(self, tile_size, overlap=0, full_coverage=False):
-        self._tile_size = tile_size
-        self._overlap = overlap
+    def __init__(self, tile_size, label_names, ratio=1, overlap=0, full_coverage=False):
+        self._tile_size = tile_size*ratio
+        self._overlap = overlap*ratio
         self._full_coverage = full_coverage
+        self._label_names= label_names
 
     def __call__(self, annotations: List[Annotation]):
-        tiled_annotations = []
+        new_annotations = []
         index=0
         for annotation in annotations:
-            if annotation.type == 'point':
-                tiled_annotations.append(annotation)
+            if annotation.label.name not in self._label_names:
+                new_annotations.append(annotation)
                 continue
-
+                
             x1, y1, x2,y2 = annotation.bounds
             for x in range(x1, x2, self._tile_size-self._overlap):
                 for y in range(y1, y2, self._tile_size-self._overlap):
                     box_poly = geometry.box(x, y, x+self._tile_size, y+self._tile_size)
                     if not self._full_coverage or box_poly.within(annotation):
-                        tiled_annotations.append(Annotation.create(
+                        new_annotations.append(Annotation.create(
                             index=index,
-                            type="polygon",
+                            type=annotation.type,
                             coordinates=box_poly.exterior.coords,
-                            label={"name": "tissue", "value": 1}
+                            label=annotation.label.todict(),
                                 ))
                         index += 1
 
-        return tiled_annotations
+        return new_annotations
