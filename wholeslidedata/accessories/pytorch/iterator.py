2f6080dc624ec2ca2c61df3b7f812e633486ac13
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-10-20 12:53:29 +0200

    use cuda

--- wholeslidedata/accessories/pytorch/iterator.py
+++ wholeslidedata/accessories/pytorch/iterator.py
@@ -2,25 +2,13 @@ import torch
 from wholeslidedata.iterators.batchiterator import BatchIterator
 
 # set dtype in create_batch_iterator to supported types e.g., int32, int16, int8, uint8
-
-
 class TorchBatchIterator(BatchIterator):
     def __next__(self):
-        if self._info_queue is not None:
-            x_batch, y_batch, info = super().__next__()
-            if len(x_batch.shape) == 5:
-                x_batch = x_batch.transpose(1, 0, 4, 2, 3).astype("float32")
-            else:
-                x_batch = x_batch.transpose(0, 3, 1, 2).astype("float32")
-
-            if len(x_batch.shape) == 5:
-                y_batch = y_batch.transpose(1, 0, 4, 2, 3).astype("float32")
-            elif len(x_batch.shape) == 4:
-                y_batch = y_batch.transpose(0, 3, 1, 2).astype("float32")
-
-            return (
-                [torch.from_numpy(x).to(torch.device("cuda:0")) for x in x_batch],
-                [torch.from_numpy(y).to(torch.device("cuda:0")) for y in y_batch],
-                info,
-            )
-        raise ValueError("torch iterator works only with info")
+        x_batch, y_batch, info = super().__next__()
+        x_batch = x_batch.transpose(1, 0, 4, 2, 3).astype("float32")
+        y_batch = y_batch.transpose(1, 0, 4, 2, 3).astype("float32")
+        return (
+            [torch.from_numpy(x).cuda() for x in x_batch],
+            [torch.from_numpy(y).cuda() for y in y_batch],
+            info,
+        )
