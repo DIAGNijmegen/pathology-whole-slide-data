f4686bcdc8d9b4c3db42ca452d633527fdfd7abf
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-01-28 12:06:52 +0100

    update tests

--- wholeslidedata/annotation/parsers.py
+++ wholeslidedata/annotation/parsers.py
@@ -1,6 +1,5 @@
 from wholeslidedata.interoperability.asap.parser import AsapAnnotationParser
 from wholeslidedata.interoperability.virtum.parser import VirtumAsapAnnotationParser
-from wholeslidedata.interoperability.s3.parser import S3AsapAnnotationParser
 from wholeslidedata.annotation.parser import WholeSlideAnnotationParser, MaskAnnotationParser
 
 DEFAULT_PARSERS = {
@@ -16,5 +15,4 @@ PARSERS = {
     'mask': MaskAnnotationParser,
     'asap': AsapAnnotationParser,
     'virtum-asap': VirtumAsapAnnotationParser,
-    's3asap': S3AsapAnnotationParser,
 }
