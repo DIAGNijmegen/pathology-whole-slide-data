c68a9bfc4b7f2a1b36ebbf99a5b9cb0875c790be
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-02-02 19:36:28 +0100

    format load batches script

--- scripts/load_batches.py
+++ scripts/load_batches.py
@@ -1,15 +1,17 @@
 from pathlib import Path
-import numpy as np
+
 import click
+import numpy as np
 
 
 @click.command()
 @click.option("--batch_folder", type=Path, required=True)
 def main(batch_folder: Path):
-    batches = sorted(list(Path(batch_folder).glob('*.npz')))
+    batches = sorted(list(Path(batch_folder).glob("*.npz")))
     for batch in batches:
         loaded_batch = np.load(batch, allow_pickle=True)
         x_batch, y_batch, info = loaded_batch.values()
+
 
 if __name__ == "__main__":
     main()
