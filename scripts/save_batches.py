1e9e00aba17a5c56030afdbb517f01dd66c62ba1
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-02-03 09:31:49 +0100

    add print statements

--- scripts/save_batches.py
+++ scripts/save_batches.py
@@ -1,9 +1,8 @@
-from pathlib import Path
 import time
+from pathlib import Path
 
 import click
 import numpy as np
-
 
 from wholeslidedata.iterators import create_batch_iterator
 
@@ -23,6 +22,7 @@ def main(
     number_of_batches: int,
     dtype: str,
 ):
+    t1_init_iterator = time.time()
     print("Initializing Batch Iterator...")
     with create_batch_iterator(
         mode=mode,
@@ -31,15 +31,24 @@ def main(
         cpus=cpus,
         buffer_dtype=dtype,
     ) as iterator:
+        print("INIT iterator time:", time.time() - t1_init_iterator)
+
+        t1_init_producers = time.time()
 
         output_folder = output_folder / mode
         print(f"Creating output folder: {output_folder}")
         output_folder.mkdir(exist_ok=True, parents=True)
+
         print("Initializing Batch Producers...")
+        t1_batch_collection = None
         for index, (x_batch, y_batch, info) in enumerate(iterator):
+            if index == 0:
+                print("INIT producers time:", time.time() - t1_init_producers)
+            t1_batch_collection = time.time()
             output_path = output_folder / f"{index}_batch.npz"
             print(f"Writing Batch {index}: {output_path}")
             np.savez(output_path, x_batch=x_batch, y_batch=y_batch, info=info)
+        print("Batch collection time", time.time() - t1_batch_collection)
 
 
 if __name__ == "__main__":
