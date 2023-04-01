54ea005545450bf9c89960f4c0d52a6d679ce24d
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-04-01 14:26:24 +0200

    update nnunet iterator

--- wholeslidedata/interoperability/nnunet/iterator.py
+++ wholeslidedata/interoperability/nnunet/iterator.py
@@ -1,8 +1,8 @@
 from wholeslidedata.iterators.batchiterator import BatchIterator
 
-class WholeSlidennUnetBatchIterator(BatchIterator):
+class WholeSlidePlainnnUnetBatchIterator(BatchIterator):
     def __next__(self):
         x_batch, y_batch, _ = super().__next__()
         x_batch = x_batch.transpose(0,3,1,2).astype('float32')
         y_batch = y_batch.astype('int16')
-        return {'data': x_batch, 'target': y_batch}
+        return {'data': x_batch, 'seg_all': y_batch}
