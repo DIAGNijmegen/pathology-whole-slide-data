f7fb6700f2c45f25f00f48bb6866f694274d9218
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-04-12 09:34:28 +0200

    fix batch iterator test

--- tests/batch_iterator_test.py
+++ tests/batch_iterator_test.py
@@ -1,12 +1,19 @@
+import os
 from pathlib import Path
-from wholeslidedata.iterators.batchiterator import create_batch_iterator
 
 from pytest_cov.embed import cleanup_on_sigterm
+
+from wholeslidedata.iterators.batchiterator import create_batch_iterator
+
 cleanup_on_sigterm()
 
 
 def test_batch_iterator():
-    with create_batch_iterator(user_config=Path(__file__).parent/"user_config.yml", mode='training', number_of_batches=5) as batch_iterator:
+    os.environ["TMP"] = "/tmp"
+    with create_batch_iterator(
+        user_config=Path(__file__).parent / "user_config.yml",
+        mode="training",
+        number_of_batches=5,
+    ) as batch_iterator:
         for x_batch, y_batch, info in batch_iterator:
             pass
-
