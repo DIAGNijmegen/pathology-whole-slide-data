264dada3f7f6400bb17b316a8d450c34873a64d2
JoeySpronck <60735928+JoeySpronck@users.noreply.github.com>
2024-10-17 15:45:35 +0200

    Update imagewriter.py with tile and coordinate checks and corrections (#65)
    * Update imagewriter.py with tile and coordinate checks and corrections
    
    * Update imagewriter.py improved coorindate checks
    
    * Cropping of patches that are partially exeeding the dimensions of the mask wasnt needed. As long as the tile size is correct this tile will be written correctly
    
    * removed reduntant comments

--- wholeslidedata/interoperability/asap/imagewriter.py
+++ wholeslidedata/interoperability/asap/imagewriter.py
@@ -58,11 +58,37 @@ class WholeSlideImageWriterBase(Writer, MultiResolutionImageWriter):
         Writer.__init__(self, callbacks)
         MultiResolutionImageWriter.__init__(self)
 
+    def _tile_and_coordinate_checks_and_corrections(self, tile, coordinates):
+        if tile.shape != self._tile_shape:
+            raise TileShapeError(
+                f"Tile shape {tile.shape} does not match expected shape {self._tile_shape}"
+            )
+        if len(tile.shape) != 2 and len(tile.shape) != 3:
+            raise TileShapeError(
+                f"Invalid tile shape provided: {tile.shape}, tile shape should contain at 2 or 3 dimensions"
+            )
+        if len(self._tile_shape) != 2 and len(self._tile_shape) != 3:
+            raise TileShapeError(
+                f"Invalid tile shape initialized: {self._tile_shape}, tile shape should contain at 2 or 3 dimensions"
+            )
+        if coordinates: 
+            x, y = coordinates
+            if x % self._tile_shape[0] != 0 or y % self._tile_shape[1] != 0:
+                raise CoordinateError(
+                    f"Coordinates {coordinates} are not multiples of the tile size {self._tile_shape[:2]}"
+                )
+            if x >= self._dimensions[0] or y >= self._dimensions[1]:
+                print(f"Tile's upper left coordinates {coordinates} are completely outside the dimensions {self._dimensions}... Skipping tile...")
+                return None
+        return tile
+
     def write_tile(self, tile, coordinates=None, mask=None):
         tile = self._apply_tile_callbacks(tile)
         tile = self._mask_tile(tile, mask)
         tile = self._crop_tile(tile)
-        self._write_tile_to_image(tile, coordinates)
+        tile = self._tile_and_coordinate_checks_and_corrections(tile, coordinates)
+        if tile is not None:
+            self._write_tile_to_image(tile, coordinates)
 
     def _write_tile_to_image(self, tile, coordinates):
         if coordinates:
